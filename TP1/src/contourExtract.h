b6603c07f71a8d1dca4fd6b32eef5971778feaea
QuentinLisack <quentin@lisack.eu>
2017-03-05 21:46:44 +0100

    extractionde contours codée

---
+++ TP1/src/contourExtract.h
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "pgm.h"
+
+#ifndef CONTOUREXTRACT
+#define CONTOUREXTRACT
+
+double** extract(double **im, int nl, int nc);
+
+#endif
--- TP1/src/testIdealFilter.c
+++ TP1/src/testIdealFilter.c
@@ -1,5 +1,6 @@
 #include "pgm.h"
 #include "libFT.h"
+#include "contourExtract.h"
 
 main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du resultat . */
     int nl, nc;
@@ -11,8 +12,13 @@ main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du
 
 	res = applyIdealFilter(im, &nl, &nc, r1, r2);
 
-    ecritureimagepgm(av[2], imdouble2uchar(res, nl, nc), nl, nc);
+    //ecritureimagepgm(av[2], imdouble2uchar(res, nl, nc), nl, nc);
+    
+    double** contours = extract(res, nl, nc);
+    
+    ecritureimagepgm(av[2], imdouble2uchar(contours, nl, nc), nl, nc);
     
     libere_image(res);
     libere_image(im);
+    libere_image(contours);
 }
